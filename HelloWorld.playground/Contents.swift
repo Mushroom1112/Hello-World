f49d3667dd19ccdd343829b0ed4a440f0f14c111
Mushroom1112 <giorgi.amiranashvili6@gmail.com>
2025-05-19 13:13:06 +0400

    addingGreeting/Comments

--- HelloWorld.playground/Contents.swift
+++ HelloWorld.playground/Contents.swift
@@ -1,3 +1,12 @@
-import UIKit
 
-var greeting = "Hello, playground"
+// 🚀 Launching Swift mission... Please fasten your seatbelts!
+
+print("Hello World")  // 👽 First contact has been made. Be polite!
+
+/*
+ 🔍
+ We've discovered that comments
+ are secret whispers between you and the code.
+ You can write as much nonsense here as you want,
+ and the compiler will politely ignore it.
+*/
